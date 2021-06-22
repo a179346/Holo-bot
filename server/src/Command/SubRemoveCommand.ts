88551cea15d13cf70b49949bb9b50a6efc9abe7e
a179346 <r122164152@gmail.com>
2021-06-22 16:30:51 +0800

    implement SubRemoveCommand

--- server/src/Command/SubRemoveCommand.ts
+++ server/src/Command/SubRemoveCommand.ts
@@ -1,9 +1,20 @@
 import { Command } from '../Class/Command';
 import { CommandOption } from '../Class/CommandOption';
 import { ReplyError } from '../Class/ReplyError';
+import { ChannelNicknameDao } from '../dao/ChannelNickname';
+import { SubscriptionDao } from '../dao/Subscription';
 
-const SubRemoveCommand = new Command('rm', 'Unsubscribe a hololive member. Stop receiving his/her live notification.', async (msg, serviceSet, messages) => {
-  throw new ReplyError('service not yet implemented.');
+interface SubRemoveOption {
+  nickname: string;
+}
+
+const SubRemoveCommand = new Command('rm', 'Unsubscribe a hololive member. Stop receiving his/her live notification.', async (msg, serviceSet, messages, body: SubRemoveOption) => {
+  const channelNicknameVal = await ChannelNicknameDao.get(body.nickname);
+  if (!channelNicknameVal)
+    throw new ReplyError('Holomem not found: ' + body.nickname);
+
+  await SubscriptionDao.remove(msg.channel.id, channelNicknameVal.channel.id);
+  msg.channel.send('Subscription removed: 【' + channelNicknameVal.channel.name + '】');
 });
 
 SubRemoveCommand.addOption(new CommandOption('nickname', [ 'name', 'n' ], 'holomem name', true));
--- server/src/dao/Subscription.ts
+++ server/src/dao/Subscription.ts
@@ -8,14 +8,27 @@ class SubscriptionDao {
     return TypeOrmConnection.connection.getRepository(subscription);
   }
 
+  public async remove (discord_channel_id: string, channelId: number): Promise<void> {
+    await this.repository.delete({
+      discord_channel_id,
+      channel: { id: channelId }
+    });
+  }
+
   public async insert (discord_channel_id: string, channelId: number): Promise<subscription> {
-    const sub = new subscription();
+    const sub = await this.repository.findOne({
+      discord_channel_id,
+      channel: { id: channelId }
+    });
+    if (sub) return sub;
 
-    sub.discord_channel_id = discord_channel_id;
-    sub.channel = new channel();
-    sub.channel.id = channelId;
+    const model = new subscription();
 
-    return await this.repository.save(sub);
+    model.discord_channel_id = discord_channel_id;
+    model.channel = new channel();
+    model.channel.id = channelId;
+
+    return await this.repository.save(model);
   }
 }
 
