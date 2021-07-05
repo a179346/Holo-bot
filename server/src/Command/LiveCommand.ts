1c70078b134b3c80f206a0b918ef14732fe06e40
a179346 <r122164152@gmail.com>
2021-07-05 17:01:03 +0800

    add LiveListSubcommand

--- server/src/Command/LiveCommand.ts
+++ server/src/Command/LiveCommand.ts
@@ -1,11 +1,13 @@
 import { NestedCommand } from '../Class/NestedCommand';
 import { LiveGetSubcommand } from './Live/LiveGetSubcommand';
+import { LiveListSubcommand } from './Live/LiveListSubcommand';
 
 const LiveCommand = new NestedCommand({
   name: 'live',
   description: 'Information about live, upcoming and recently ended streams.'
 }, [
   LiveGetSubcommand,
+  LiveListSubcommand,
 ]);
 
 export  {
