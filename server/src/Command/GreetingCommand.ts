be96671a4ca4b42997e946c00af439864bc9d338
a179346 <r122164152@gmail.com>
2021-07-17 17:51:25 +0800

    add GreetingCommand

---
+++ server/src/Command/GreetingCommand.ts
@@ -0,0 +1,35 @@
+import { Command } from '../Class/Command';
+import { CommandOptionType } from '../interface/CommandOptionType';
+import { ReplyError } from '../Class/ReplyError';
+import { ChannelNicknameDao } from '../dao/ChannelNickname';
+import { Lib } from '../lib/common';
+
+export const GreetingCommand = new Command({
+  name: 'greeting',
+  description: 'Hololive greetings.',
+  options: [ {
+    name: 'name',
+    description: 'holomem\'s name Ex. "okayu"',
+    type: CommandOptionType.STRING,
+    required: true,
+  }, ]
+}, async (interaction, options) => {
+  const name = options.get('name')?.value;
+  if (typeof name !== 'string')
+    throw new ReplyError('Invalid Options: "name"');
+
+  const channelNicknameVal = await ChannelNicknameDao.get(name);
+  if (!channelNicknameVal)
+    throw new ReplyError('Holomem not found: ' + name);
+  if (!channelNicknameVal.channel.greeting)
+    throw new ReplyError('Sorry :( Greeting from 【' + name + '】 is currently not available.');
+
+  const prefix = Lib.getChannelPrefix(channelNicknameVal.channel);
+  let content = prefix + channelNicknameVal.channel.greeting ;
+  content += '            From: ' + interaction.user.toString();
+
+  interaction.reply({
+    content,
+    ephemeral: false,
+  });
+});
